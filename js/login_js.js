eda7ce97136e7989851d0248b484ba667f799a85
akarsh <akarshagri121@gmail.com>
2022-08-24 14:01:19 +0530

    added

--- js/login_js.js
+++ js/login_js.js
@@ -1,14 +1,26 @@
-function change_pwd()
+function change_pwd() {
+    var chng = document.getElementById("chng");
+    var but = document.getElementById("pwd");
+    if (but.type == "password") {
+        chng.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
+        but.type = "text";
+    }
+    else {
+        chng.innerHTML = '<i class="fa fa-eye"></i>';
+        but.type = "password";
+    }
+}
+function fg_pwd() {
+    var chng = document.getElementById("box1");
+    fetch('../html/forgot_psw.html')
+        .then(response => response.text())
+        .then(text => document.getElementById('box1').innerHTML = text);
+}
+function login()
 {
-    var chng=document.getElementById("chng");
-    var but=document.getElementById("pwd");
-    if(but.type=="password")
-    {
-        chng.innerHTML='<i class="fa-solid fa-eye-slash"></i>';
-        but.type="text";
-    }
-    else{
-        chng.innerHTML='<i class="fa fa-eye"></i>';
-        but.type="password";
-    }
+    var chng = document.getElementById("box1");
+    fetch('../html/login_set.html')
+        .then(response => response.text())
+        .then(text => document.getElementById('box1').innerHTML = text);
+
 }
